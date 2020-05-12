c49712394ee6131069f64190a31e3992f0ab749f
Mangeshi <thisismangeshi@gmail.com>
2020-05-12 15:45:59 +0530

    Adds stopwatch

--- stopwatch.py
+++ stopwatch.py
@@ -9,32 +9,31 @@ root.geometry("240x140")
 root.configure(bg='black')
 style.configure('TButton',font=('arial',10,'bold'),borderwidth="5")
 
-second=0;minute=0;hour=0;stop=0
+second=0;minute=0;hour=0;stop=True
 
 def start():
     global second,minute,hour,stop
+    
     second += 1
     if second==60:
         minute += 1
         second =0
     if minute == 60:
         hour += 1
-        minute=0
-    if stop ==0:
-        lbl=Label(root,text='%i:%i:%i'%(hour,minute,second),font=('arial',30,'bold'),foreground='red',width=10)
+        minute=0    
+    if stop==True:
+        lbl=Label(root,text='%s:%s:%s'%(hour,minute,second),font=('arial',30,'bold'),foreground='red',width=10,background='black')
         lbl.after(1000,start)
         lbl.place(x=50,y=60)
 
-
-    
-def stop():
+ 
+def stopped():
     global stop
-    stop = 1
-
-
-
+    stop=False
+    
+         
 start_button=Button(root,text="Start",command=start)
-stop_button=Button(root, text="Stop",command=stop)
+stop_button=Button(root, text="Stop",command=stopped)
 
 start_button.grid(row=0, column=0)
 stop_button.grid(row=0 ,column=1)
